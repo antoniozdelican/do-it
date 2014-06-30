a81f78a158096f0eb6f8a118b3c666fe7c19b8a4
Antonio Zdelican <antonio.zdelican@gmail.com>
2014-06-30 23:03:44 +0200

    removed late enumeration.

--- app/enumeration/status.rb
+++ app/enumeration/status.rb
@@ -1,6 +1,5 @@
 class Status < Enumeration::Base
   values  completed: { id: 1, name: "Completed" },
           pending: { id: 2, name: "Pending" },
-          late: { id: 3, name: "Late" },
-          canceled: { id: 4, name: "Canceled" }
+          canceled: { id: 3, name: "Canceled" }
 end
