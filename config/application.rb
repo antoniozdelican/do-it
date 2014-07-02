dd2986b3338ef18aa6017b6e395198d6c4be2998
Antonio Zdelican <antonio.zdelican@gmail.com>
2014-07-02 02:57:36 +0200

    cleaned up the code.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module DoIt
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # added for removing warning
+    I18n.enforce_available_locales = true
   end
 end
